0e43ab568b9e00741aa00065d833050ce79e7860
Paige Williams and Tanvi Garg <tanvi@>
2019-02-21 08:42:27 -0800

    Set up environment; working.

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Map from './Map';
+import { Switch, Route } from 'react-router';
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Lato');
+    font-family: 'Lato', sans-serif;
+  }
+`;
+
+
+function App(){
+  return (
+    <div>
+      <GlobalStyles />
+      <Switch>
+        <Route exact path='/' component={Map} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Map(){
+  return(
+    <div>
+      <h1>Map works</h1>
+    </div>
+  )
+}
+
+export default Map;
